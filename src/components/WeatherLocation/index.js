82dffcd35f2bc54174ae758e566557d61ab36daf
Daniel Gomez <danielgomez@MacBook-Pro-de-ione-2.local>
2019-01-28 21:47:43 -0500

    Creacion de Weather-app, aplicacion la cual muestra informacion climatica segun la ubicacion

---
+++ src/components/WeatherLocation/index.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress'
+import {PropTypes} from 'prop-types';
+import Location from './Location';
+import WeatherData from '../WeatherLocation/WeatherData/index';
+import './styles.css';
+import transformWeather from './../../services/transformWeather';
+import {api_weather} from './../../constants/api_url';
+
+
+
+class WeatherLocation extends Component {
+
+    constructor(props){
+        super(props);
+        const  { city } = props;
+        this.state = {
+            city,
+            data: null,
+        };
+        console.log("constructor");
+
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+        this.handleUpdateClick();
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate");
+
+
+    }
+
+  
+    handleUpdateClick = () => {
+        fetch(api_weather).then(resolve =>{
+            return resolve.json();
+        }).then(data =>{
+            const newWeather = transformWeather(data);
+            console.log(newWeather);
+                this.setState({
+                    data: newWeather
+                })
+            console.log(data);
+        })
+        
+    };
+
+    render(){
+        console.log("render");
+        const {city, data } = this.state;
+        return (
+            <div className="weatherLocationCont">
+                <Location city={city}></Location>
+                {data ?
+                <WeatherData data={data}></WeatherData> : 
+                <CircularProgress size={50}/>
+                }
+        </div>
+        );
+    }
+    
+    
+};
+
+
+WeatherLocation.propTypes = {
+    city : PropTypes.string.isRequired,
+}
+
+
+export default WeatherLocation;
---
+++ src/constants/api_url.js
@@ -0,0 +1,5 @@
+const  location = "Cali,co";
+const api_key = "e22d6538a59730b8e36dafc9ff026f36";
+const  url_base_weather = "http://api.openweathermap.org/data/2.5/weather/"; 
+
+export const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`; 
